3ee896a6a141b454abc835236cd3688350ab4433
Koya <koya@koya.scrgg.cc>
2023-05-08 19:55:01 -0400

    Update function descriptoins to help with understanding how to use them more.

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -6,17 +6,17 @@ class SpotifyInterface:
 
 
     def search_track_id(self, track_id: str):
-        """ Search a track on spotify. `track_id` is a spotify track ID"""
+        """ Search a track on spotify. `track_id` is a spotify track ID, returns a SpotifyTrack object."""
         return self.spotify_handler.search_id(track_id)
 
     def search_album_tracks(self, album_id: str):
-        """ Search an albums tracks on spotify. `album_id` is a spotify track ID"""
+        """ Search an albums tracks on spotify. `album_id` is a spotify track ID, returns a SpotifyAlbum object"""
         return self.spotify_handler.search_album_tracks(album_id)
 
     def search_many_tracks(self, tracks_ids: list):
-        """ Search for multiple tracks at once. `track_ids` is a list full of spotify track IDs """
+        """ Search for multiple tracks at once. `track_ids` is a list full of spotify track IDs, returns a list of SpotifyTrack objects """
         return self.spotify_handler.search_many_tracks(album_id)
 
     def search_playlist_tracks(self, playlist_id: str):
-        """ Search a playlists tracks on spotify. `playlist_id` is a spotify playlist ID"""
+        """ Search a playlists tracks on spotify. `playlist_id` is a spotify playlist ID, returns a list of SpotifyTrack objects"""
         return self.spotify_handler.search_playlist_tracks(album_id)
